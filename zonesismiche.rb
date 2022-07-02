feec93e339f7e7f4f32cc67d2e8b1deb73ae962a
Ferdinando Traversa <ferdi.traversa@gmail.com>
2022-07-02 14:58:06 +0200

    Altri aggiustamenti, rimuovo le frazioni

--- zonesismiche.rb
+++ zonesismiche.rb
@@ -20,7 +20,6 @@ unless File.exist? "#{__dir__}/.config"
     end
 end
 userdata = File.open("#{__dir__}/.config", "r").to_a
-
 wikipedia = MediawikiApi::Client.new("https://it.wikipedia.org/w/api.php")
 wikipedia.log_in(userdata[0].strip, userdata[1].strip)
 active = userdata[2].strip == "y" ? true : false
@@ -33,7 +32,7 @@ c = 0
 tot = 0
 n = 0
 unless File.exist? "#{__dir__}/lista.txt"
-    url = "https://petscan.wmflabs.org/?format=json&doit=&categories=Comuni%20d%27Italia&depth=8&project=wikipedia&lang=it&templates_yes=divisione%20amministrativa"
+    url = "https://petscan.wmflabs.org/?format=json&doit=&categories=Comuni%20d%27Italia&depth=8&project=wikipedia&lang=it&templates_yes=divisione%20amministrativa&negcats=Frazioni%20comunali%20d%27Italia"
     petscan = HTTParty.get(url, timeout: 1000).to_h["*"][0]["a"]["*"]
     lista = File.open("#{__dir__}/lista.txt", "w")
     lista.write(petscan.to_json)
@@ -119,10 +118,10 @@ begin
             title = petscan.find { |e| e["title"].include?(row[3].gsub(" ", "_").gsub("-", "_"))}["title"]
         elsif petscan.find { |e| e["title"].include?(row[3].gsub(" ", "_").gsub("_", "-"))} != nil
             title = petscan.find { |e| e["title"].include?(row[3].gsub(" ", "_").gsub("_", "-"))}["title"]
-        elsif petscan.find { |e| e["title"].include?(row[3].gsub(" ", "_").gsub(" - ", "-"))} != nil
-            title = petscan.find { |e| e["title"].include?(row[3].gsub(" ", "_").gsub(" - ", "-"))}["title"]
-        elsif petscan.find { |e| e["title"].include?(row[3].gsub(" ", "_").gsub("-", " - "))} != nil
-            title = petscan.find { |e| e["title"].include?(row[3].gsub(" ", "_").gsub("-", " - "))}["title"]
+        elsif petscan.find { |e| e["title"].include?(row[3].gsub(" - ", "-").gsub(" ", "_"))} != nil
+            title = petscan.find { |e| e["title"].include?(row[3].gsub(" - ", "-").gsub(" ", "_"))}["title"]
+        elsif petscan.find { |e| e["title"].include?(row[3].gsub("-", " - ").gsub(" ", "_"))} != nil
+            title = petscan.find { |e| e["title"].include?(row[3].gsub("-", " - ").gsub(" ", "_"))}["title"]
         elsif petscan.find { |e| e["title"].include?(row[3].gsub("d'","di ").gsub(" ", "_"))} != nil 
             title = petscan.find { |e| e["title"].include?(row[3].gsub("d'","di ").gsub(" ", "_"))}["title"]
         else
